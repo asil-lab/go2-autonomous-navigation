1d3bef5d40b4cee25cfe797ed6c21c9f35e6f3fc
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-07-17 00:04:18 +0000

    Update docker files and dependencies

--- .devcontainer/setup_aliases.sh
+++ .devcontainer/setup_aliases.sh
@@ -1,4 +1,4 @@
 alias sfox='source /opt/ros/foxy/setup.bash'
 alias sws='source /home/lava-tube-mapping/install/setup.bash'
 
-alias rosdep_install='rosdep update && rosdep install --from-paths src --ignore-src -r -y --rosdistro foxy'
+alias rosdep_install='rosdep update --rosdistro foxy && rosdep install --from-paths src --ignore-src -r -y --rosdistro foxy'
