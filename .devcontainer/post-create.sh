9ba3c5319ef113f9b12ce34fc2e627ae046842c2
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-07-27 18:52:53 +0000

    Add ROS2 workspace, set up aliases, and READMEs

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -22,21 +22,12 @@ catkin config --workspace "${ROOT}/ros1_ws" \
     --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --
 echo "ROS1 workspace setup and configured."
 
+# Build the ROS1 workspace
+catkin build --workspace "${ROOT}/ros1_ws" &>/dev/null
+echo "ROS1 workspace built."
+
 # Configure the ROS2 workspace
+source $ROS2_INSTALL_PATH/setup.bash
 
-
-# Configure the ROS1 Catkin workspace
-# if [ ! -d "${ROOT}/ros1_ws/src" ]; then
-#     # Create the ROS1 Catkin workspace and src folder
-#     mkdir -p ${ROOT}/ros1_ws/src
-#     cd ${ROOT}/ros1_ws
-
-#     # Setup catkin workspace
-#     catkin init --workspace "${ROOT}/ros1_ws" &>/dev/null
-
-#     # Configure the workspace
-#     catkin config --workspace "${ROOT}/ros1_ws" --extend /opt/ros/noetic \
-#         --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-# else
-#     echo "ROS1 Catkin workspace already exists."
-# fi
+# Source aliases
+echo "source ${ROOT}/.devcontainer/setup_aliases.sh" >> ~/.bashrc
