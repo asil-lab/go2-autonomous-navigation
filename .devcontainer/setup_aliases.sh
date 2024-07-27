3dc27ba0abfc2ace1016ccc3eb30787727d6c539
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-07-27 21:45:30 +0000

    Fix gitignore and aliases

--- .devcontainer/setup_aliases.sh
+++ .devcontainer/setup_aliases.sh
@@ -3,5 +3,5 @@ alias noetic='source /opt/ros/noetic/setup.bash && echo "ROS1 sourced."'
 alias foxy='source /opt/ros/foxy/setup.bash && echo "ROS2 sourced."'
 
 # Source ROS workspaces
-alias ws1='cd /home/lava-tube-mapping/ros1_ws && sros1 && source devel/setup.bash && echo "ROS1 workspace sourced."'
-alias ws2='cd /home/lava-tube-mapping/ros2_ws && sros2 && source install/setup.bash && echo "ROS2 workspace sourced."'
+alias ws1='cd /home/lava-tube-mapping/ros1_ws && noetic && source devel/setup.bash && echo "ROS1 workspace sourced."'
+alias ws2='cd /home/lava-tube-mapping/ros2_ws && foxy && source install/setup.bash && echo "ROS2 workspace sourced."'
