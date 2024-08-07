2859c24eb7ee6abfa2c62268ada7e976e611b939
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-08-07 13:53:14 +0200

    implement pointcloud buffer and filter nodes to familiarize environment for navigation (#19)
    * Add post create command bash script in Docker
    
    * Add ROS2 Foxy and ros1_bridge to connect ROS1 with ROS2 (#3)
    
    * Modify image to install ROS1 Noetic and ROS2 Foxy
    
    * Improve Dockerfile by copying ROS1 noetic artifact into ROS2 Foxy container
    
    * Create separate ROS1 workspace, and implement ROS1 workspace setup and configuration
    
    * Add ROS2 workspace, set up aliases, and READMEs
    
    * Remove unitree_ros in ros1_ws
    
    * Add unitree_ros as submodule in ros1_ws
    
    * Add ros1_bridge, and fix aliases
    
    * Fix gitignore and aliases
    
    * Update repo with ROS1 and ROS2 workspaces, and ros1_bridge
    
    * Add post create command bash script in Docker
    
    * Add ROS2 Foxy and ros1_bridge to connect ROS1 with ROS2 (#3)
    
    * Modify image to install ROS1 Noetic and ROS2 Foxy
    
    * Improve Dockerfile by copying ROS1 noetic artifact into ROS2 Foxy container
    
    * Create separate ROS1 workspace, and implement ROS1 workspace setup and configuration
    
    * Add ROS2 workspace, set up aliases, and READMEs
    
    * Remove unitree_ros in ros1_ws
    
    * Add unitree_ros as submodule in ros1_ws
    
    * Add ros1_bridge, and fix aliases
    
    * Fix gitignore and aliases
    
    * Remove original unitree_ros in ROS1 workspace
    
    * Add forked unitree_ros in ROS1 workspace
    
    * Fix submodule unitree_ros and install full desktop ROS noetic
    
    * Add velodyne sensor in go2
    
    * Create ROS2 package to filter raw pointcloud for navigation
    
    * Create launch file, and fix parameters in ltm_pointcloud_filter
    
    * Implement plane segmentation, and fix getting parameters in ltm_pointcloud_filter
    
    * Implement cluster and 3d bounding boxes including visualization in ltm_pointcloud_filter
    
    * Add go2 urdf and meshes to create pointcloud of robot for cluster extraction
    
    * Create separate h/cpp-files for ground plane removal
    
    * Temporarily save changes attempt to add URDF pointcloud
    
    * Create robot cluster removal h/cpp files
    
    * Update unitree_ros and modify Dockerfile
    
    * Create ROS2 package to buffer raw LIDAR pointcloud into meaningful data
    
    * Implement DAE mesh to pointcloud converter in robot_cluster_removal
    
    * Deactivate canTransform in ltm_pointcloud_filter as it did not work as intended
    
    * Refactor ltm_pointcloud_filter
    
    * Fix parameter handling in ltm_pointcloud_buffer, and add throttling info
    
    * Refactor and parameterize ltm_pointcloud_buffer
    
    * Refactor and add pseudocoding in robot_cluster_removal
    
    * Implement drawing robot meshes on pointcloud input
    
    * Modify Docker files
    
    * Refactor ltm_pointcloud_buffer with parameters
    
    * Create exploration stack
    
    * Fix timing issue in transform in ltm_pointcloud_buffer
    
    * Implement pointcloud to laserscan
    
    * Create separate hpp/cpp for downsampling pointcloud using voxel grid filter
    
    * Temporary fix of voxel grid filter, and implement crude robot cluster removal
    
    * Move ltm_pointcloud_buffer and ltm_pointcloud_filter in ltm_exploration
    
    * Fix voxel_grid_filter in ltm_pointcloud_filter
    
    * Implement sync in pointCloudCallback with sportmodestate to filter loose points when lying
    
    * Revert "Implement sync in pointCloudCallback with sportmodestate to filter loose points when lying"
    
    This reverts commit effd30c54a950e1ebe5a07ac81e4951b5976a405.
    
    * Rename GroundPlaneRemoval to GroundPlaneSegmentation, and fix VoxelGridFilter
    
    * Fix minor mistakes
    
    * Implement statistical outlier removal in ltm_pointcloud_filter
    
    * Parameterize crop box filter in robot_cluster_removal
    
    * Fix parameterization in robot_cluster_removal
    
    * Fix parameterization of statistical outlier removal

---
+++ ros2_ws/src/ltm_exploration/ltm_exploration_core/launch/exploration.launch.py
@@ -0,0 +1,49 @@
+"""
+Project Lava Tube Mapping, Technical University of Delft.
+Author: Alexander James Becoy @alexanderjamesbecoy
+"""
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    
+    # Pointcloud buffer node
+    pointcloud_buffer_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('ltm_pointcloud_buffer'), 
+                         'launch', 'pointcloud_buffer.launch.py')
+        )
+    )
+    
+    # Pointcloud filter node
+    pointcloud_filter_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('ltm_pointcloud_filter'), 
+                         'launch', 'pointcloud_filter.launch.py')
+        ),
+        launch_arguments=[('in_simulation', 'false')]
+    )
+    
+    # Pointcloud-to-laserscan node
+    pointcloud_to_laserscan_config = os.path.join(
+        get_package_share_directory('ltm_exploration_core'), 'config', 'parameters.yaml')
+    pointcloud_to_laserscan_node = Node(
+        package='pointcloud_to_laserscan',
+        executable='pointcloud_to_laserscan_node',
+        name='pointcloud_to_laserscan_node',
+        output='screen',
+        remappings=[('cloud_in', 'point_cloud/filtered'),],
+        # parameters=[pointcloud_to_laserscan_config],
+    )
+    
+    return LaunchDescription([
+        pointcloud_buffer_node,
+        pointcloud_filter_node,
+        pointcloud_to_laserscan_node,
+    ])
