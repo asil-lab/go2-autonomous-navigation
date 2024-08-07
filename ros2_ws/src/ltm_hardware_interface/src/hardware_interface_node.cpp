5ca8a25c3876c28880c0c0b57504d21ea4f1a2e7
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-08-07 13:18:50 +0000

    Revert "Revert "Create separate hpp/cpp for processing odometry""
    This reverts commit fd55f4defde022cd4faefb562e14cfa22e32cc33.

--- ros2_ws/src/ltm_hardware_interface/src/hardware_interface_node.cpp
+++ ros2_ws/src/ltm_hardware_interface/src/hardware_interface_node.cpp
@@ -14,12 +14,7 @@ HardwareInterfaceNode::HardwareInterfaceNode()
   // Initialize processing classes
   m_front_camera_processing = std::make_shared<FrontCameraProcessing>();
   m_joint_state_processing = std::make_shared<JointStateProcessing>();
-
-  // Initialize the messages
-  initializeWorldToBaseTransformMsg();
-
-  // Initialize the transform broadcaster
-  m_tf_broadcaster = std::make_shared<tf2_ros::TransformBroadcaster>(this);
+  m_odom_processing = std::make_shared<OdomProcessing>(this->shared_from_this());
 
   // Create subscriptions
   m_low_state_sub = this->create_subscription<unitree_go::msg::LowState>(
@@ -56,34 +51,32 @@ HardwareInterfaceNode::~HardwareInterfaceNode()
   m_front_video_360p_pub.reset();
   m_front_video_720p_pub.reset();
 
-  // Reset transform broadcaster
-  m_tf_broadcaster.reset();
-
   // Reset processing classes
   m_front_camera_processing.reset();
   m_joint_state_processing.reset();
-
-  // Reset msg pointers
-  m_world_to_base_transform_msg.reset();
+  m_odom_processing.reset();
 
   RCLCPP_WARN(this->get_logger(), "LTM Hardware Interface Node shutting down.");
 }
 
 void HardwareInterfaceNode::lowStateCallback(const unitree_go::msg::LowState::SharedPtr msg)
 {
-  m_joint_state_processing->updateJointStateMsg(msg->motor_state);
-  m_joint_state_pub->publish(*(m_joint_state_processing->getJointStateMsg()));
+    RCLCPP_INFO_THROTTLE(this->get_logger(), *this->get_clock(), 5000, "/lowstate message received!");
+  updateJointStateMsg(msg->motor_state);
+  publishJointState();
 }
 
 void HardwareInterfaceNode::sportModeStateCallback(const unitree_go::msg::SportModeState::SharedPtr msg)
 {
-  updateWorldToBaseTranslation(msg->position);
-  updateWorldToBaseOrientation(msg->imu_state.quaternion);
-  broadcastWorldToBaseTransform();
+  // Update the odometry transform from base to world
+  m_odom_processing->updateOdomTranslation(msg->position);
+  m_odom_processing->updateOdomOrientation(msg->imu_state.quaternion);
+  m_odom_processing->broadcastOdomTransform();
 }
 
 void HardwareInterfaceNode::pointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
 {
+  // Change the frame_id of the point cloud message to "radar"
   sensor_msgs::msg::PointCloud2 point_cloud_msg = *msg;
   point_cloud_msg.header.frame_id = "radar";
   m_point_cloud_pub->publish(point_cloud_msg);
@@ -91,52 +84,12 @@ void HardwareInterfaceNode::pointCloudCallback(const sensor_msgs::msg::PointClou
 
 void HardwareInterfaceNode::frontVideoCallback(const unitree_go::msg::Go2FrontVideoData::SharedPtr msg)
 {
+  // Publish the front camera video messages in 180p, 360p, and 720p
   m_front_camera_processing->updateFrontCameraMsgs(msg, this->now());
 
   m_front_video_180p_pub->publish(*(m_front_camera_processing->getFrontCamera180pMsg()));
   m_front_video_360p_pub->publish(*(m_front_camera_processing->getFrontCamera360pMsg()));
   m_front_video_720p_pub->publish(*(m_front_camera_processing->getFrontCamera720pMsg()));
-}
-
-void HardwareInterfaceNode::updateWorldToBaseTranslation(const std::array<float, 3>& translation)
-{
-  m_world_to_base_transform_msg->transform.translation.x = translation[0];
-  m_world_to_base_transform_msg->transform.translation.y = translation[1];
-  m_world_to_base_transform_msg->transform.translation.z = translation[2];
-}
-
-void HardwareInterfaceNode::updateWorldToBaseOrientation(const std::array<float, 4>& orientation)
-{
-  m_world_to_base_transform_msg->transform.rotation.w = orientation[0];
-  m_world_to_base_transform_msg->transform.rotation.x = orientation[1];
-  m_world_to_base_transform_msg->transform.rotation.y = orientation[2];
-  m_world_to_base_transform_msg->transform.rotation.z = orientation[3];
-}
-
-void HardwareInterfaceNode::broadcastWorldToBaseTransform()
-{
-  m_world_to_base_transform_msg->header.stamp = this->now();
-  m_tf_broadcaster->sendTransform(*m_world_to_base_transform_msg);
-}
-
-void HardwareInterfaceNode::initializeWorldToBaseTransformMsg()
-{
-  // Initialize the IMU transform message
-  m_world_to_base_transform_msg = std::make_shared<geometry_msgs::msg::TransformStamped>();
-
-  // Set the parent and child frames
-  m_world_to_base_transform_msg->header.frame_id = "world";
-  m_world_to_base_transform_msg->child_frame_id = "base";
-
-  // Set the transform values to zero
-  m_world_to_base_transform_msg->transform.translation.x = 0.0;
-  m_world_to_base_transform_msg->transform.translation.y = 0.0;
-  m_world_to_base_transform_msg->transform.translation.z = 0.0;
-
-  m_world_to_base_transform_msg->transform.rotation.w = 0.0;
-  m_world_to_base_transform_msg->transform.rotation.x = 0.0;
-  m_world_to_base_transform_msg->transform.rotation.y = 0.0;
-  m_world_to_base_transform_msg->transform.rotation.z = 0.0;
 }
 
 int main(int argc, char * argv[])
