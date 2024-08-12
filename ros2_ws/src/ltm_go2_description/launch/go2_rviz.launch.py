a6988fb15ddb3300f4400e563ed2161c8d017287
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-08-12 14:27:06 +0200

    Move robot_state_publisher to hardware_interface

--- ros2_ws/src/ltm_go2_description/launch/go2_rviz.launch.py
+++ ros2_ws/src/ltm_go2_description/launch/go2_rviz.launch.py
@@ -14,42 +14,13 @@ from launch_ros.actions import Node
 
 def generate_launch_description() -> LaunchDescription:
     
-    declared_arguments = [
-        DeclareLaunchArgument(
-            name="use_sim_true",
-            default_value="false",
-            description="Use simulation time",
-            choices=["true", "false"],
-        ),
-    ]
-    
-    use_sim_time = LaunchConfiguration("use_sim_true", default="false")
-    
-    # Get the URDF file location
-    urdf_location = os.path.join(
-        get_package_share_directory("ltm_go2_description"), "urdf", "go2_description.urdf")
-    
-    # Read the URDF file content
-    with open(urdf_location, 'r') as infp:
-        robot_description = infp.read()
-    
-    return LaunchDescription(declared_arguments + [
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[{
-                'use_sim_time': use_sim_time, 
-                'robot_description': robot_description
-            }],
-            arguments=[urdf_location],
-        ),
+    return LaunchDescription([
         Node(
             package='rviz2',
             executable='rviz2',
             name='rviz2',
             output='screen',
-            arguments=['-d', os.path.join(get_package_share_directory('ltm_go2_description'), 'rviz', 'go2.rviz')],
+            arguments=['-d', os.path.join(
+                get_package_share_directory('ltm_go2_description'), 'rviz', 'go2.rviz')],
         ),
     ])
---
+++ ros2_ws/src/ltm_hardware_interface/launch/go2_driver.launch.py
@@ -0,0 +1,48 @@
+"""
+Project Lava Tube Mapping, Technical University of Delft.
+Author: Alexander James Becoy @alexanderjamesbecoy
+"""
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    # URDF file location
+    urdf_location = os.path.join(
+        get_package_share_directory("ltm_go2_description"), "urdf", "go2_description.urdf")
+    
+    # Read the URDF file content
+    with open(urdf_location, 'r') as infp:
+        robot_description = infp.read()
+
+    # Go2 driver node
+    go2_driver_node = Node(
+        package='ltm_hardware_interface',
+        executable='hardware_interface_node',
+        name='hardware_interface_node',
+        output='screen',
+    )
+
+    # Robot state publisher node
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': robot_description,
+            'use_sim_time': False,
+        }],
+        arguments=[urdf_location],
+    )
+
+    return LaunchDescription([
+        go2_driver_node,
+        robot_state_publisher_node,
+    ])
