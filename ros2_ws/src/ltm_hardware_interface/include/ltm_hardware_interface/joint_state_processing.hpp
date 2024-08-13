509a244c8fd09ac493e4248b723cf381210ef8ec
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-08-13 11:57:01 +0200

    Convert joint_state_processing to a ROS2 node

--- ros2_ws/src/ltm_hardware_interface/include/ltm_hardware_interface/joint_state_processing.hpp
+++ ros2_ws/src/ltm_hardware_interface/include/ltm_hardware_interface/joint_state_processing.hpp
@@ -15,6 +15,12 @@
 #include <string>
 #include <vector>
 
+#define ROS_NODE_NAME "joint_state_processing_node"
+#define ROS_SUB_TOPIC "lowstate"
+#define ROS_SUB_QUEUE_SIZE 10
+#define ROS_PUB_TOPIC "joint_states"
+#define ROS_PUB_QUEUE_SIZE 10
+
 #define MOTOR_SIZE 20
 #define JOINT_STATE_FRAME_ID "joint_link"
 
@@ -31,26 +37,45 @@
 #define JOINT_RL_THIGH_IDX    10
 #define JOINT_RL_CALF_IDX     11
 
+#define JOINT_FR_HIP_NAME     "FR_hip_joint"
+#define JOINT_FR_THIGH_NAME   "FR_thigh_joint"
+#define JOINT_FR_CALF_NAME    "FR_calf_joint"
+#define JOINT_FL_HIP_NAME     "FL_hip_joint"
+#define JOINT_FL_THIGH_NAME   "FL_thigh_joint"
+#define JOINT_FL_CALF_NAME    "FL_calf_joint"
+#define JOINT_RR_HIP_NAME     "RR_hip_joint"
+#define JOINT_RR_THIGH_NAME   "RR_thigh_joint"
+#define JOINT_RR_CALF_NAME    "RR_calf_joint"
+#define JOINT_RL_HIP_NAME     "RL_hip_joint"
+#define JOINT_RL_THIGH_NAME   "RL_thigh_joint"
+#define JOINT_RL_CALF_NAME    "RL_calf_joint"
+
 namespace LTM
 {
-  class JointStateProcessing
+  class JointStateProcessing : public rclcpp::Node
   {
     public:
       JointStateProcessing();
       ~JointStateProcessing();
 
+    private:
+      void lowStateCallback(const unitree_go::msg::LowState::SharedPtr msg);
+      void publishJointStateMsg() const;
+
       void updateJointStateMsg(
         const std::array<unitree_go::msg::MotorState, MOTOR_SIZE>& motor_state);
-
       sensor_msgs::msg::JointState::SharedPtr getJointStateMsg() const;
 
-    private:
+      void initializeROS();
       void initializeJointStateMsg();
       void initializeJointIdx();
       std::vector<double> initializeZeroVector(const unsigned long int size) const;
 
       sensor_msgs::msg::JointState::SharedPtr m_joint_state_msg;
       std::vector<int> m_joint_idx;
+
+      rclcpp::Subscription<unitree_go::msg::LowState>::SharedPtr m_low_state_sub;
+      rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr m_joint_state_pub;
 
   };  // class JointStateProcessing
 } // namespace LTM
