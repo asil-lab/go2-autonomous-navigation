efa15f2ad61206a61bcfbfec334353a206a0dd55
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-09-05 11:24:17 +0200

    Create state.py

---
+++ ros2_ws/src/ltm_state_machine/ltm_state_machine/states.py
@@ -0,0 +1,14 @@
+""" Here contains the states.
+Author: Alexander James Becoy
+Revision: 1.0
+Date: 2024-09-05
+"""
+
+class State:
+
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
+    def transition(self):
+        pass
