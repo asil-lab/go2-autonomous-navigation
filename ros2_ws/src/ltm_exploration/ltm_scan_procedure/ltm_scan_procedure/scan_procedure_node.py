3446bca389d8419138ed4fe1331baece04fbc154
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-09-27 14:57:39 +0200

    Add RecordEnvironment service in ltm_scan_procedure

--- ros2_ws/src/ltm_exploration/ltm_scan_procedure/ltm_scan_procedure/scan_procedure_node.py
+++ ros2_ws/src/ltm_exploration/ltm_scan_procedure/ltm_scan_procedure/scan_procedure_node.py
@@ -10,10 +10,10 @@ from rclpy.executors import MultiThreadedExecutor
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 from tf2_ros import Buffer, TransformListener
 
-from geometry_msgs.msg import PoseStamped, Point, Quaternion
+from geometry_msgs.msg import Point, Quaternion
 from sensor_msgs.msg import PointCloud2, Image
 from std_msgs.msg import Empty, String
-from ltm_shared_msgs.srv import GetImage, GetPointCloud, NavigateToPose
+from ltm_shared_msgs.srv import GetImage, GetPointCloud, NavigateToPose, RecordEnvironment
 
 import os
 from typing import Any
@@ -49,6 +49,7 @@ class ScanProcedureNode(Node):
         self.configure_navigate_to_pose_client()
         self.configure_get_image_client()
         self.configure_get_pointcloud_client()
+        self.configure_record_environment_service()
         self.configure_tf_listener()
 
         self.get_logger().info('Scan procedure node has been initialized.')
@@ -56,6 +57,12 @@ class ScanProcedureNode(Node):
     def trigger_callback(self, msg) -> None:
         self.get_logger().info('Trigger has been received.')
         self.perform_scan()
+
+    def record_environment_callback(self, request, response) -> RecordEnvironment.Response:
+        self.get_logger().info('Record environment service has been called.')
+        self.perform_scan()
+        response.success = True
+        return response
 
     def publish_gesture(self, gesture: str) -> None:
         """Publishes the gesture to the gesture topic.
@@ -343,6 +350,15 @@ class ScanProcedureNode(Node):
         while not self.get_pointcloud_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Service %s not available, waiting again...' % \
                                    (self.get_parameter('get_pointcloud_service_name').value))
+
+    def configure_record_environment_service(self) -> None:
+        """Configures the client to the record_environment service."""
+        self.declare_parameter('record_environment_service_name', 'record_environment')
+
+        self.record_environment_callback_group = MutuallyExclusiveCallbackGroup()
+        self.record_environment_service = self.create_service(
+            RecordEnvironment, self.get_parameter('record_environment_service_name').value, 
+            self.record_environment_callback, callback_group=self.record_environment_callback_group)
 
     def configure_tf_listener(self) -> None:
         """Configures the tf listener for the node."""
