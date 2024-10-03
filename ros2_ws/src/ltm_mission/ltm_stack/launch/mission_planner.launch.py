daf4533dac6ce019b61527142e00351215f457ec
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-10-03 15:38:35 +0200

    Create launch file for high level nodes

---
+++ ros2_ws/src/ltm_mission/ltm_stack/launch/mission_planner.launch.py
@@ -0,0 +1,40 @@
+"""
+Project Lava Tube Mapping, Technical University of Delft.
+Author: Alexander James Becoy @alexanderjamesbecoy
+"""
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    # Declare launch arguments
+
+    # Go2 driver launch file
+    navigation_planner_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('ltm_navigation_core'), 
+                'launch', 'planner.launch.py')
+        )
+    )
+
+    # Scan procedure node
+    scan_procedure_node = Node(
+        package='ltm_scan_procedure',
+        executable='scan_procedure_node',
+        name='scan_procedure_node',
+        output='screen',
+    )
+
+    return LaunchDescription([
+        # Launch arguments
+        # Launch files
+        navigation_planner_node,
+        scan_procedure_node,
+    ])
