8b6bdef1684ded6dd79bdcbfe2cf884d08bd14fe
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-10-11 15:08:57 +0200

    Create aliases for Go2

--- setup.sh
+++ setup.sh
@@ -13,8 +13,12 @@ export LTM_MAPS_DIRECTORY="${LTM_ROS2_WS}/src/ltm_mission/maps"
 
 # Create aliases
 alias ltm="cd ${HOME}/lava-tube-mapping/ros2_ws && source ../setup.sh && source /opt/ros/foxy/setup.bash && source src/unitree_ros2/setup.sh"
-alias ltms="ltm && source install/setup.sh"
+alias ltms="ltm && source install/setup.bash"
 alias ltmbp="colcon build --symlink-install --packages-skip cyclonedds"
 alias ltmbps="colcon build --symlink-install --packages-select"
 alias ltmbpu="colcon build --symlink-install --packages-up-to"
 alias ltmbc="rm -rf build/ install/ log/ && colcon build --symlink-install --packages-skip cyclonedds"
+
+# Create aliases on Go2
+alias go2_setup="cd ${HOME}/lava-tube-mapping/ros2_ws && source /opt/ros/foxy/setup.bash"
+alias go2_source="go2_setup && source install/setup.bash"
