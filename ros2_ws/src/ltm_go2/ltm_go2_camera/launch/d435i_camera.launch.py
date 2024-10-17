2b5d4cd7ddfac7ad3e3a87cfbb0ff5e6c7ed3537
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-10-17 12:25:17 +0200

    Modify d435i_camera launch file

--- ros2_ws/src/ltm_go2/ltm_go2_camera/launch/d435i_camera.launch.py
+++ ros2_ws/src/ltm_go2/ltm_go2_camera/launch/d435i_camera.launch.py
@@ -7,30 +7,44 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch import LaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    # Realsense d435i camera node
-    realsense_node = Node(
-        package='realsense2_camera',
-        executable='realsense2_camera_node',
-        name='realsense2_camera_node',
-        output='screen',
-	namespace='',
-        parameters=[
-            {
-                'camera_name': 'd435i_camera',
-                'camera_namespace': '',
-                # 'enable_depth': True,
-                # 'enable_infra': True,
-                'enable_sync': True,
-                'enable_rgbd': True,
-                'enable_gyro': True,
-                'enable_accel': True,
-                'publish_tf': True,
-                'pointcloud.enable': True
-            }
+    # # Realsense d435i camera node
+    # realsense_node = Node(
+    #     package='realsense2_camera',
+    #     executable='realsense2_camera_node',
+    #     name='realsense2_camera_node',
+    #     output='screen',
+	# namespace='',
+    #     parameters=[
+    #         {
+    #             'camera_name': 'd435i_camera',
+    #             'camera_namespace': '',
+    #             # 'enable_depth': True,
+    #             # 'enable_infra': True,
+    #             'enable_sync': True,
+    #             'enable_rgbd': True,
+    #             'enable_gyro': True,
+    #             'enable_accel': True,
+    #             'publish_tf': True,
+    #             'pointcloud.enable': True
+    #         }
+    #     ],
+    # )
+    realsense_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('realsense2_camera'), 
+                'launch', 'rs_launch.launch.py')
+        ),
+        launch_arguments=[
+            {'camera_name': 'realsense2_camera'},
+            {'camera_namespace': ''},
+            {'enable_infra1': 'true'},
+            {'enable_infra2': 'true'},
+            {'publish_tf': 'true'},
         ],
     )
 
