315bcd1e30c83398e7468fdc7f727bd1f1209c29
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-11-04 16:48:58 +0100

    Create minimal viable product

--- auxiliary_sensing/src/main.cpp
+++ auxiliary_sensing/src/main.cpp
@@ -1,18 +1,53 @@
 #include <Arduino.h>
+#include <hp_BH1750.h>
+#include <Adafruit_SHT31.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+struct MISO {
+  u_int32_t lux;
+  u_int32_t temperature;
+  u_int32_t humidity;
+};
+
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+hp_BH1750 BH1750;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  bool avail = BH1750.begin(BH1750_TO_GROUND);
+  if (!avail) {
+    Serial.println("No BH1750 sensor found!");
+    while (true) {};
+  }
+  Serial.println("BH1750 sensor found!");
+
+  if (! sht31.begin(0x44)) {
+    Serial.println("Couldn't find SHT31");
+    while (1) delay(1);
+  }
+  Serial.println("SHT31 sensor found!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  BH1750.start();
+  float lux = BH1750.getLux();
+  Serial.print("Lux: "); Serial.println(lux);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  float t = sht31.readTemperature();
+  float h = sht31.readHumidity();
+
+  if (! isnan(t)) {
+    Serial.print("Temp *C = "); Serial.print(t); Serial.print("\t\t");
+  } else { 
+    Serial.println("Failed to read temperature");
+  }
+
+  if (! isnan(h)) {
+    Serial.print("Hum. % = "); Serial.println(h);
+  } else { 
+    Serial.println("Failed to read humidity");
+  }
+
+  delay(1000);
 }
