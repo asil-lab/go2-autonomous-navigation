31efac55b0f2df757033807a6a121faee803f790
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-11-10 17:52:28 +0100

    Implement bias calculations for linear and angular velocities

--- ros2_ws/src/utilities/odometry_bias_estimation/odometry_bias_estimation/odometry_bias_estimator_node.py
+++ ros2_ws/src/utilities/odometry_bias_estimation/odometry_bias_estimation/odometry_bias_estimator_node.py
@@ -11,6 +11,7 @@ from geometry_msgs.msg import TwistStamped
 
 from collections import deque
 from scipy import stats
+from scipy.spatial.transform import Rotation as R
 
 class OdometryBiasEstimatorNode(Node):
 
@@ -19,6 +20,7 @@ class OdometryBiasEstimatorNode(Node):
         self.initialize_queues()
         self.initialize_bias_publisher()
         self.initialize_sport_mode_state_subscriber()
+        self.initialize_timer()
         self.get_logger().info('Odometry Bias Estimator Node has been initialized.')
 
     def sport_mode_state_callback(self, msg):
@@ -29,10 +31,8 @@ class OdometryBiasEstimatorNode(Node):
 
         # Update the linear and angular velocities
         for i in range(3):
-            self.linear_velocities[i].append(msg.position[i])
-            # self.angular_velocities[i].append(msg.angular_velocity[i])
-
-        self.publish_bias()
+            self.linear_translation[i].append(msg.position[i])
+            self.angular_orientation[i].append(msg.imu_state.rpy[i])
 
     def publish_bias(self):
         # Check if the queues are full
@@ -41,25 +41,30 @@ class OdometryBiasEstimatorNode(Node):
         
         # Calculate the bias
         time_stamps = list(self.time_stamps)
-        linear_velocities = [list(v) for v in self.linear_velocities]
-        # angular_velocities = [list(v) for v in self.angular_velocities]
+        linear_translation = [list(v) for v in self.linear_translation]
+        angular_orientation = [list(v) for v in self.angular_orientation]
 
-        # Calculate the bias for x
-        slope, intercept, r_value, p_value, std_err = stats.linregress(time_stamps, linear_velocities[0])
-        # self.get_logger().info(f'Linear Velocity (x) Slope: {slope}, Intercept: {intercept}, R-Value: {r_value}, P-Value: {p_value}, Std-Error: {std_err}')
+        # Calculate the slopes of the linear and angular velocities
+        linear_velocities = [stats.linregress(time_stamps, linear_translation[i])[0] for i in range(3)]
+        angular_velocities = [stats.linregress(time_stamps, angular_orientation[i])[0] for i in range(3)]
 
         # Create the message
         msg = TwistStamped()
         msg.header.stamp = self.get_clock().now().to_msg()
-        msg.twist.linear.x = slope
+        msg.twist.linear.x = linear_velocities[0]
+        msg.twist.linear.y = linear_velocities[1]
+        msg.twist.linear.z = linear_velocities[2]
+        msg.twist.angular.x = angular_velocities[0]
+        msg.twist.angular.y = angular_velocities[1]
+        msg.twist.angular.z = angular_velocities[2]
 
         # Publish the message
         self.bias_publisher.publish(msg)
 
     def initialize_queues(self, queue_size=1000):
         self.time_stamps = deque(maxlen=queue_size)
-        self.linear_velocities = [deque(maxlen=queue_size) for _ in range(3)]
-        # self.angular_velocities = [deque(maxlen=queue_size) for _ in range(3)]
+        self.linear_translation = [deque(maxlen=queue_size) for _ in range(3)]
+        self.angular_orientation = [deque(maxlen=queue_size) for _ in range(3)]
 
     def initialize_bias_publisher(self):
         self.bias_publisher = self.create_publisher(TwistStamped, 'odometry_bias', 10)
@@ -68,6 +73,9 @@ class OdometryBiasEstimatorNode(Node):
         self.sport_mode_state_subscription = self.create_subscription(
             SportModeState, 'sportmodestate', self.sport_mode_state_callback, 10)
         self.current_time = self.get_clock().now()
+
+    def initialize_timer(self, timer_period=1.0):
+        self.timer = self.create_timer(timer_period, self.publish_bias)
 
 def main():
     rclpy.init()
