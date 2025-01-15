d17f84cfc4a7168049bdd3d7a63378a2b4c0593e
Alexander James Becoy <bec_alexanderjames@outlook.com>
2025-01-15 20:46:09 +0100

    Create ltm_data_collection to collect data for verification

---
+++ ros2_ws/src/ltm_mission/ltm_data_collection/ltm_data_collection/data_collection_node.py
@@ -0,0 +1,18 @@
+"""
+TODO:
+- [ ] Feasibility: demonstration
+- [ ] Multiple trials: success/fail (RMSE), reachability to target
+- [ ] Time-to-reach target
+- [ ] Implement data collection on autonomous navigation
+
+Assumptions:
+- [ ] Waypoint resolution (distance between waypoints)
+
+"""
+
+def main():
+    print('Hi from ltm_data_collection.')
+
+
+if __name__ == '__main__':
+    main()
